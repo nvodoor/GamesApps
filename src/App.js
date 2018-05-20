3f62808e6eeddce289b892e5a184571facb741f2
Neil Vodoor <nvodoor@gmail.com>
2018-05-19 17:31:01 -0700

    Added CSS.

--- src/App.js
+++ src/App.js
@@ -8,28 +8,9 @@ import Scorelist from './Routes/Scorelist/Scorelist';
 import Battleship from './Routes/Battleship/Battleship';
 import Ind from './Routes/Index/Index';
 
-import { Route, BrowserRouter, NavLink, Switch, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter,Switch, Redirect } from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    comment1: 'Hi, lets play a game. Click on me to start.',
-    comment2: "Here are the games to play: tic-tac-toe, rock-paper-scissors, Battleship",
-    toggle: 'on'
-  }
-
-  onCommentChange = () => {
-    if (this.state.toggle === 'on') {
-      document.getElementById('1').innerHTML = this.state.comment2
-      this.setState({
-        toggle: 'off'
-      })
-    } else {
-      document.getElementById('1').innerHTML = this.state.comment1
-      this.setState({
-        toggle: 'on'
-      })
-    }
-  }
 
   render() {
     return (
--- src/Routes/Battleship/Battleship.js
+++ src/Routes/Battleship/Battleship.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react';
 import '../../App.css';
 import BattleBoard from './BattleBoard.js';
+import './Battleship.css';
 
 class Battleship extends Component {
 
     state = {
         playerLocs: [],
         computerLocs: [],
-        turn: 'player',
+        turn: '',
         moves: 0,
-        picks: []
+        picks: [[],[],[]],
+        winner: ''
     }
 
     componentDidMount () {
@@ -29,8 +31,6 @@ class Battleship extends Component {
                 this.state.computerLocs.push(num)
             }
         }
-        console.log(this.state.playerLocs)
-        console.log(this.state.computerLocs)
     }
 
     handleClick = (e) => {
@@ -38,22 +38,78 @@ class Battleship extends Component {
         if (this.state.playerLocs.includes(parseInt(e.target.id))) {
             e.target.innerHTML = "*";
             e.target.style = "color: blue; font-size: 30px"
+            this.state.picks[0].push('p1')
         }
         if (this.state.computerLocs.includes(parseInt(e.target.id))) {
             e.target.innerHTML = "*";
             e.target.style = "color: red; font-size: 30px"
+            this.state.picks[1].push('c1')
         }
         if (!this.state.playerLocs.includes(parseInt(e.target.id)) && !this.state.computerLocs.includes(parseInt(e.target.id))) {
             e.target.innerHTML = "X"
             e.target.style = "color: black"
+            this.state.picks[2].push('X')
+        }
+        if (this.state.picks[0].length === 3) {
+            this.state.winner = 'Player'
+        }
+        if (this.state.picks[1].length === 3) {
+            this.state.winner = 'Computer'
+        }
+        if (this.state.turn === 'player' || this.state.turn === '') {
+            this.setState({
+                turn: 'computer'
+            })
+        } else {
+            this.setState({
+                turn: 'player'
+            })
         }
     }
 
     render () {
+        let turns = (
+            <div className="startbox">
+                <p className="para">Shall we begin? Player1...</p>
+            </div>
+        )
+
+        if (this.state.turn === 'player') {
+            let listPlayer = this.state.playerLocs.map((number) =>
+                <li>{number}</li>
+            );
+            turns = (
+                <div className="turnbox">
+                    <p className="para">This turn is for the player.</p>
+                    <ul className="paraplus">{listPlayer}</ul>
+                </div>
+            )
+        } 
+        if (this.state.turn === 'computer') {
+            let listComputer = this.state.computerLocs.map((number) =>
+                <li>{number}</li>
+            );
+            turns = (
+                <div className="turnbox">
+                    <p className="para">This turn is for the computer.</p>
+                    <ul className="paraplus">
+                        {listComputer}
+                    </ul>
+                </div>
+            )
+        }
+        if (this.state.winner !== '') {
+            return (
+                <div>
+                    <p className="para">The winner is: {this.state.winner}</p>
+                </div>
+            )
+        }
         return (
             <div>
                 <p className="para"> Welcome to Battleship.</p>
                 <BattleBoard clicked={this.handleClick}/>
+                {turns}
             </div>
         )
     }
